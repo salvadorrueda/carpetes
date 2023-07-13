0dc89fac2f76e0017893d0e90e0d24a65d261dc2
Salvador Rueda Pau <salvador.rueda@gmail.com>
2023-07-13 17:06:02 +0200

    Nou codi recursiu

--- carpetes.py
+++ carpetes.py
@@ -1,32 +1,33 @@
-import os
 import xml.etree.ElementTree as ET
+import os
+import sys
 
-def crear_carpetes_des_de_xml(ruta_xml):
-    # Analitzar el fitxer XML
-    tree = ET.parse(ruta_xml)
+def crear_carpetas(directori, ruta_actual=''):
+    nom_directori = directori.get('nom')
+    ruta_completa = os.path.join(ruta_actual, nom_directori)
+
+    # Crear la carpeta en el sistema de archivos
+    try:
+        os.makedirs(ruta_completa)
+        print(f'Se ha creado la carpeta "{ruta_completa}"')
+    except FileExistsError:
+        print(f'La carpeta "{ruta_completa}" ya existe')
+
+    # Recorrer los subdirectorios
+    for subdirectori in directori.findall('directori'):
+        crear_carpetas(subdirectori, ruta_completa)
+
+def interpretar_xml(archivo):
+    # Parsear el archivo XML
+    tree = ET.parse(archivo)
     root = tree.getroot()
 
-    # Recórrer els elements "directori"
-    for directori_elem in root.findall("./directori"):
-        nom_directori = directori_elem.get("nom")
-        crear_directori(nom_directori)
+    # Llamar a la función para crear las carpetas
+    for directori in root.findall('directori'):
+        crear_carpetas(directori)
 
-        # Recórrer els elements "subdirectori" dins de cada "directori"
-        for subdirectori_elem in directori_elem.findall("./directori"):
-            nom_subdirectori = subdirectori_elem.get("nom")
-            crear_directori(os.path.join(nom_directori, nom_subdirectori))
+# Obtener el nombre del archivo XML como argumento de línea de comandos
+nombre_archivo = sys.argv[1]
 
-def crear_directori(nom_directori):
-    # Comprovar si el directori ja existeix.
-    if os.path.exists(nom_directori):
-        print(f"El directori '{nom_directori}' ja existeix.")
-    else:
-        # Crear el directori
-        os.mkdir(nom_directori)
-        print(f"S'ha creat el directori '{nom_directori}'.")
-
-# Ruta del fitxer XML
-ruta_xml = "carpetes_M7.xml"
-
-# Cridar la funció per crear les carpetes.
-crear_carpetes_des_de_xml(ruta_xml)
+# Llamar a la función para interpretar el XML
+interpretar_xml(nombre_archivo)
